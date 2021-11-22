c3a864c1178f378464576e468c8915aa4856a8cc
Kayk Santos <kaykarrudasantos2018@gmail.com>
2021-11-22 20:17:52 -0300

    Adicionando os arrays

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Atividade_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] chamada;
+            chamada = new string[10];
+            string[] nomes;
+            nomes = new string[10];
+                          
+        }
+    }
+}
