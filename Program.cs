40ac8f004d468329565c5b9c840dcf1c9463ae42
Beazika <beniteen1316@gmail.com>
2021-11-23 20:29:29 -0300

    beatriz

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Atividade_CSharp
             do{
                 nomes = new string[10] {"[ 01 ] Antony S.", "[ 02 ] Bruce B.", "[ 03 ] Clint B.", "[ 04 ] Gamora Z.", "[ 05 ] Michele J.", "[ 06 ] Natasha R.", "[ 07 ] Nebulosa Z.", "[ 08 ] Peter P.", "[ 09 ] Stephen S.", "[ 10 ] Wanda M."};
                 Console.WriteLine("\nMenu de registro");
-                Console.WriteLine("[ 1 ] Registrar presença\n[ 2 ] Consultar presença\n[ 3 ] Lista de alunos\n[ 0 ] Sair");
+                Console.WriteLine("[ 1 ] Registrar presença\n[ 2 ] Consultar presença\n[ 0 ] Sair");
                 Console.Write("Escolha a opção desejada: ");
                 menu = int.Parse(Console.ReadLine());
                 Console.Clear();
@@ -83,9 +83,18 @@ namespace Atividade_CSharp
 
                         }
                     break;
-		    }
+
+                    case 0:
+                        sair1 = true;
+                    break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente");
+                    break;
+                }
             }while(sair1 != true);
             
         }
     }
 }
+
